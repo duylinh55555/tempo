43b8a3cd5b1218abf1465fae1de92982e9bb0102
Linh <linh9920duy@gmail.com>
2020-10-27 14:30:28 +0700

    Add project files.

---
+++ WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello";
+        }
+
+        public string Get(string id)
+        {
+            return "Hello " + id;
+        }
+    }
+}
